919a264feb164f6c182ccb11eb99648c13f8ab4b
jigneshnts-ux <jigneshnts@gmail.com>
2026-06-09 15:28:23 +0530

    Add in-app feature progress dashboard

---
+++ progress-dashboard.js
@@ -0,0 +1,12 @@
+(() => {
+  const groups = [
+    ['Canvas & Workspace', 'Partial foundation'],
+    ['Selection', 'Foundation module added'],
+    ['Crop & Transform', 'Foundation module added'],
+    ['Layers', 'Engine and panel foundation added'],
+    ['Drawing & Painting', 'Brush registry added'],
+    ['Shapes & Pen', 'Shape foundation added'],
+    ['Text & Typography', 'Basic controls added'],
+    ['Color Adjustments', 'Basic controls added'],
+    ['Filters & Effects', 'Registry foundation added'],
+    ['Masks', 'Registry foundation added'],
