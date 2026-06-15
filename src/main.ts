d5936f0f5a8d98fc383de70fbc464d65956f3c6d
jigneshnts-ux <jigneshnts@gmail.com>
2026-06-15 21:57:33 +0530

    Add CreatorX DOM entry

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { App } from './App';
+
+const mount = document.getElementById('root');
+
+if (!mount) {
+  throw new Error('CreatorX root element was not found.');
+}
+
+mount.innerHTML = '';
+mount.appendChild(App());
