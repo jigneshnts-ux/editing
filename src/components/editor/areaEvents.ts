c6540a4d8dd9c6e173b7afbe1b4af76270a598cf
jigneshnts-ux <jigneshnts@gmail.com>
2026-06-16 17:36:57 +0530

    Add area action event constant

--- src/components/editor/areaEvents.ts
+++ src/components/editor/areaEvents.ts
@@ -1,3 +1,4 @@
 export const areaUpdatedEvent = 'creatorx-area-updated';
 export const areaClearedEvent = 'creatorx-area-cleared';
 export const areaClearRequestEvent = 'creatorx-area-clear-request';
+export const areaActionRequestEvent = 'creatorx-area-action-request';
