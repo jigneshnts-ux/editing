4d161519be23a87d85ee273c109c8ddd14c29d92
jigneshnts-ux <jigneshnts@gmail.com>
2026-06-18 13:37:32 +0530

    Attach preset panel to editor shell

--- src/components/editor/createEditorShell.ts
+++ src/components/editor/createEditorShell.ts
@@ -5,10 +5,11 @@ import { createRightPanel } from './createRightPanel';
 import { createSceneStrip } from './createSceneStrip';
 import { createHistoryPanel } from './createHistoryPanel';
 import { createProjectPanel } from './createProjectPanel';
+import { createPresetPanel } from './createPresetPanel';
 
 export function createEditorShell(): HTMLElement {
   const root = document.createElement('section');
   root.className = 'editor-shell';
-  root.append(createTopBar(), createLeftToolbar(), createCanvasArea(), createRightPanel(), createProjectPanel(), createHistoryPanel(), createSceneStrip());
+  root.append(createTopBar(), createLeftToolbar(), createCanvasArea(), createRightPanel(), createPresetPanel(), createProjectPanel(), createHistoryPanel(), createSceneStrip());
   return root;
 }
