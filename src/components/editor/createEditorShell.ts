a91f0141e3b81ebc908552b63fc5d3b08f3ab12c
jigneshnts-ux <jigneshnts@gmail.com>
2026-06-18 14:05:15 +0530

    Add editor panel

--- src/components/editor/createEditorShell.ts
+++ src/components/editor/createEditorShell.ts
@@ -6,10 +6,11 @@ import { createSceneStrip } from './createSceneStrip';
 import { createHistoryPanel } from './createHistoryPanel';
 import { createProjectPanel } from './createProjectPanel';
 import { createPresetPanel } from './createPresetPanel';
+import { createPreviewPanel } from './createPreviewPanel';
 
 export function createEditorShell(): HTMLElement {
   const root = document.createElement('section');
   root.className = 'editor-shell';
-  root.append(createTopBar(), createLeftToolbar(), createCanvasArea(), createRightPanel(), createPresetPanel(), createProjectPanel(), createHistoryPanel(), createSceneStrip());
+  root.append(createTopBar(), createLeftToolbar(), createCanvasArea(), createRightPanel(), createPresetPanel(), createProjectPanel(), createHistoryPanel(), createPreviewPanel(), createSceneStrip());
   return root;
 }
